b51ee093da3a94829ee8fa6e3c963fb051b28035
kjy <rlawhdduq95@gmail.com>
2024-10-29 20:22:01 +0900

    최초 업로드

---
+++ src/test/java/todolist/follow/FollowApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist.follow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
